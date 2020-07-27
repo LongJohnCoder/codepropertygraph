86e13c09b653a3cb98190b7eccd6e84a4adb2ebb
Michael Pollmeier <michael@michaelpollmeier.com>
2020-07-27 14:25:00 +1200

    ControlStructure - doesn't compile any more

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/package.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/package.scala
@@ -70,7 +70,8 @@ package object language extends operatorextension.Implicits {
   implicit def toType(steps: Steps[nodes.Type]): Type = new Type(steps)
   implicit def toTypeDecl(steps: Steps[nodes.TypeDecl]): TypeDecl = new TypeDecl(steps)
   implicit def toCall(steps: Steps[nodes.Call]): OriginalCall = new OriginalCall(steps)
-  implicit def toControlStructure(steps: Steps[nodes.ControlStructure]): ControlStructure = new ControlStructure(steps)
+  implicit def toControlStructure(traversal: Traversal[nodes.ControlStructure]): ControlStructure =
+    new ControlStructure(traversal)
   implicit def toIdentifier(steps: Steps[nodes.Identifier]): IdentifierTrav = new IdentifierTrav(steps)
   implicit def toMember(steps: Steps[nodes.Member]): Member = new Member(steps)
   implicit def toMetaData(steps: Steps[nodes.MetaData]): MetaData = new MetaData(steps)
